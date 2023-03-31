5f78e976829052cd052ea71e513a4bb3c0e94446
Felipe Ruete <feruete@gmail.com>
2023-03-31 09:15:04 -0300

    treflePost

--- routes/apiTrefleRoutes.js
+++ routes/apiTrefleRoutes.js
@@ -6,7 +6,7 @@ const {
 
 const router = express.Router();
 
-router.get("/search", searchApiTrefle);
+router.post("/search", searchApiTrefle);
 router.get("/slug/:slug", dataApiTrefle);
 
 module.exports = router;
